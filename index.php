96cbb26dfabd2e58117017d080beab6889824c21
Rafaelly CMR <rafaellycmr@gmail.com>
2021-08-04 19:07:15 -0300

    comando  DELETE

--- index.php
+++ index.php
@@ -32,20 +32,14 @@ require_once("config.php");
 
 
 //Alterar um usuário
-$usuario = new Usuario();
-
-$usuario->loadById(8);
-
-$usuario->update("professor", "!@#$%¨&*");
-
-echo $usuario;
-
-
 //$usuario = new Usuario();
-
-//$usuario->loadById(7);
-
-//$usuario->delete();
-
+//$usuario->loadById(8);
+//$usuario->update("professor", "!@#$%¨&*");
 //echo $usuario;
+
+//Deletar um usuário
+$usuario = new Usuario();
+$usuario->loadById(7);
+$usuario->delete();
+echo $usuario;
  ?>
